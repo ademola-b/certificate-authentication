59d9f3df3eb2bf8f563d30b396fb66781e5bca79
demolab <bellofaisol@gmail.com>
2023-10-21 07:29:56 +0100

    :construction_worker: Models

--- certapp/models.py
+++ certapp/models.py
@@ -1,3 +1,39 @@
+import uuid
 from django.db import models
 
 # Create your models here.
+class Department(models.Model):
+    dept_id = models.UUIDField(default=uuid.uuid4, primary_key=True, unique=True, editable=False)
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return f"{self.name}"
+ 
+class Course(models.Model):
+    course_id = models.UUIDField(default=uuid.uuid4, primary_key=True, unique=True, editable=False)
+    code = models.CharField(max_length=50)
+    title = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.code} - {self.title}"
+
+class Holder(models.Model):
+    holder_id = models.UUIDField(default=uuid.uuid4, primary_key=True, unique=True, editable=False)
+    matric_no = models.CharField(max_length=50)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.CharField(max_length=100, unique=True, null=True, blank=True)
+    department = models.ForeignKey(Department, on_delete=models.DO_NOTHING)
+    course = models.ForeignKey(Course, on_delete=models.DO_NOTHING)
+    grade = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.first_name} - {self.last_name}"
+
+class Certificate(models.Model):
+    cert_id = models.UUIDField(default=uuid.uuid4, primary_key=True, unique=True, editable=False)
+    serial_number = models.CharField(max_length=20, unique=True)
+    holder = models.ForeignKey(Holder, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f"{self.holder.first_name} - {self.serial_number}"
--- certauth/settings.py
+++ certauth/settings.py
@@ -10,10 +10,14 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 
 
 # Quick-start development settings - unsuitable for production
@@ -37,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'qr_code',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +59,7 @@ ROOT_URLCONF = 'certauth.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR,],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,6 +121,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIR = [STATIC_DIR,]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
